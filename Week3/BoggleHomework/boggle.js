9b6faecee38a8062de8baedff14bb4baa4706521
Brandon LaDuke <BLADUK8617@suscorp.edu>
2017-04-10 21:35:01 -0400

    Added instructions

--- Week3/BoggleHomework/boggle.js
+++ Week3/BoggleHomework/boggle.js
@@ -1,2 +1,23 @@
-var dictionary = ["first", "second", "third"];
-var matrix = [["a", "c", "e"], ["t", "s", "o"], ["e", "n", "k"]];
+var dictionary = ["YAP","YAH","ATE","ANT","ANTRE","ANTE","ANE","ANELE","ANY","TAP","TAN","TRENCH","TREY","THY",
+"THAN","THANE","TERN","TEN","TENCH","TEL","TELE","RET","RENT","RELY","RELET","PYA","PAY","PAT",
+"PATH","PATE","PATER","PATEN","PAH","PAN","PANT","PANE","PANEL","PHAT","PHT","HYP","HAY","HAT",
+"HATE","HATER","HAP","HANT","HANCE","NAY","NAP","NAH","NTH","NET","ETA","ETH","ETNA","ERN","EYE",
+"CHAY","CHAT","CHAP","CHANT","CHANTER","CHANTEY","CELT","LET","LENT","LEY","LYNCH","LYE","LECH",
+"LEV","YET","YEN","YENTA","YECH","YECHY","VET","EYNE","EYER","EYEN","TYNE","TYE","TECHY"];
+var matrix = [["Y", "A", "T", "R"], [" P ", "H", "N", "E"] , ["X", "C", "L", "Y"] , ["S", "V", "E", "T"] ];
+
+//----------------------Homework Part 1----------------------
+//-----------------------------------------------------------
+//Create a boggle game using the above Dictionary and Matrix
+
+
+
+//----------------------Homework Part 2----------------------
+//-----------------------------------------------------------
+//Please describe:
+//1) Why it is usually suggested to declare a variable and assign an anonymous function to it.
+//2) Why this code is not working:
+//a();
+//var a = function(){
+//console.log("Hi");
+//}
